5a24e534aec44bb3773d3b6dda30ec07a0679fd0
Brad <9489141+bberk@users.noreply.github.com>
2022-09-13 15:38:21 -0400

    Update for start of 2022 season (#11)
    - Fix end of regular season missing week
    - Update python version to 3.10.6
    - Update espn_api to 0.21.1
    - Update pandas to 1.4.4

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version='0.1.0',
     author='Brad Berk',
     install_requires=[
-        'espn_api>=0.19.0',
-        'pandas==1.3.4',
+        'espn_api>=0.21.1',
+        'pandas==1.4.4',
         'numpy',
         'Django==3.2.8'
     ]
