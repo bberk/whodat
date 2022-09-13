2ab82058b731acafd9a6f64f93687234d48d676f
Bradley Berk <bradley.berk@lookout.com>
2022-09-13 09:38:15 -0400

    Update for start of 2022 season
    - Update python version to 3.10.6
    - Update espn_api to 0.21.1
    - Update pandas to 1.4.4

--- legacy/whodat.py
+++ legacy/whodat.py
@@ -5,16 +5,14 @@ from espn_api.football import League as EspnLeague
 from league import League
 
 
-league_year = 2021
-
-
 def read_config():
     config = configparser.ConfigParser()
     config.read('secrets.conf')
     return {
         'espn_league_id': int(config['DEFAULT']['espn_league_id']),
         'espn_s2': config['DEFAULT']['espn_s2'],
-        'swid': config['DEFAULT']['swid']
+        'swid': config['DEFAULT']['swid'],
+        'league_year': int(config['DEFAULT']['league_year'])
     }
 
 
@@ -22,7 +20,7 @@ if __name__ == '__main__':
     conf = read_config()
 
     espn_league = EspnLeague(league_id=conf['espn_league_id'],
-                             year=league_year,
+                             year=conf['league_year'],
                              espn_s2=conf['espn_s2'],
                              swid=conf['swid'])
 
